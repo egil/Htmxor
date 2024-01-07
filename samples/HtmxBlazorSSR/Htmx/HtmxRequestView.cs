53f47bccc4a7d575b059e2e9d67c1e1fdd9c89ab
Egil Hansen <egil@assimilated.dk>
2024-01-07 23:40:43 +0000

    feat: enable FullPageContent to reference HtmxContent
    #1

--- samples/HtmxBlazorSSR/Htmx/HtmxRequestView.cs
+++ samples/HtmxBlazorSSR/Htmx/HtmxRequestView.cs
@@ -1,36 +1,90 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace HtmxBlazorSSR.Htmx;
 
 public class HtmxRequestView : IComponent
 {
     private static readonly RenderFragment EmptyRenderFragment = builder => { };
-
-    private RenderHandle renderHandler;
+    private RenderHandle renderHandle;
 
     [Inject] private HtmxContext Context { get; set; } = default!;
 
-    [Parameter] public RenderFragment<HtmxContext>? ChildContent { get; set; }
+    [Parameter] public RenderFragment<HtmxRequestViewContext>? ChildContent { get; set; }
 
-    [Parameter] public RenderFragment<HtmxContext>? HtmxContent { get; set; }
-
-    [Parameter] public RenderFragment? RegularContent { get; set; }
-
-    public void Attach(RenderHandle renderHandle) => this.renderHandler = renderHandle;
+    public void Attach(RenderHandle renderHandle) => this.renderHandle = renderHandle;
 
     public Task SetParametersAsync(ParameterView parameters)
     {
         parameters.SetParameterProperties(this);
-
-        if (Context.Request.IsHtmxRequest && Context.Request.Trigger is not null)
+        var ctx = new HtmxRequestViewContext(Context);
+        renderHandle.Render(builder =>
         {
-            renderHandler.Render((ChildContent ?? HtmxContent)?.Invoke(Context) ?? EmptyRenderFragment);
-        }
-        else
-        {
-            renderHandler.Render((RegularContent ?? EmptyRenderFragment));
-        }
+            builder.OpenComponent<CascadingValue<HtmxRequestViewContext>>(0);
+            builder.AddComponentParameter(1, nameof(CascadingValue<HtmxRequestViewContext>.Value), ctx);
+            builder.AddComponentParameter(2, nameof(CascadingValue<HtmxRequestViewContext>.ChildContent), (ChildContent?.Invoke(ctx) ?? EmptyRenderFragment));
+            builder.CloseComponent();
+        });
+        return Task.CompletedTask;
+    }
+}
 
+public class HtmxRequestViewContext
+{
+    private static readonly RenderFragment EmptyRenderFragment = builder => { };
+
+    public HtmxContext Context { get; }
+
+    public RenderFragment HtmxContent { get; private set; } = EmptyRenderFragment;
+
+    public HtmxRequestViewContext(HtmxContext context)
+    {
+        Context = context;
+    }
+
+    internal void SetHtmxContent(RenderFragment htmxContent)
+        => HtmxContent = htmxContent;
+}
+
+public class HtmxContent : IComponent
+{
+    private RenderHandle renderHandle;
+
+    [CascadingParameter] private HtmxRequestViewContext ViewContext { get; set; } = default!;
+
+    [Parameter, EditorRequired] public required RenderFragment ChildContent { get; set; }
+
+    public void Attach(RenderHandle renderHandle) => this.renderHandle = renderHandle;
+
+    public Task SetParametersAsync(ParameterView parameters)
+    {
+        parameters.SetParameterProperties(this);
+        ViewContext.SetHtmxContent(ChildContent);
+        if (ViewContext.Context.Request.IsHtmxRequest)
+        {
+            renderHandle.Render(ChildContent);
+        }
+        return Task.CompletedTask;
+    }
+}
+
+public class FullPageContent : IComponent
+{
+    private RenderHandle renderHandle;
+
+    [CascadingParameter] private HtmxRequestViewContext ViewContext { get; set; } = default!;
+
+    [Parameter, EditorRequired] public required RenderFragment ChildContent { get; set; }
+
+    public void Attach(RenderHandle renderHandle) => this.renderHandle = renderHandle;
+
+    public Task SetParametersAsync(ParameterView parameters)
+    {
+        parameters.SetParameterProperties(this);
+        if (!ViewContext.Context.Request.IsHtmxRequest)
+        {
+            renderHandle.Render(ChildContent);
+        }
         return Task.CompletedTask;
     }
 }
