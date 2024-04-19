9051d235d7f1d86c8ff1e851402b0ce50d33b33e
Egil Hansen <egil@assimilated.dk>
2024-04-19 21:37:49 +0000

    refactor: use helper for asserting markup

--- test/Htmxor.Tests/RequestRoutingTest.cs
+++ test/Htmxor.Tests/RequestRoutingTest.cs
@@ -14,25 +14,7 @@ public class RequestRoutingTest : TestAppTestBase
         await Host.Scenario(s =>
         {
             s.Get.Url("/normal-and-hx");
-            s.ContentShouldBeHtml("""
-                <!DOCTYPE html>
-                <html lang="en">
-                <head>
-                    <meta charset="utf-8">
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                    <base href="/">
-                    <link rel="stylesheet" href="app.css">
-                    <link rel="stylesheet" href="Htmxor.TestApp.styles.css">
-                    <script defer src="/js/htmx.min.js"></script>
-                    <script defer src="_content/Htmxor/htmxor.js"></script>
-                    <meta name="htmx-config" content="{&quot;selfRequestsOnly&quot;:true,&quot;antiforgery&quot;:{&quot;formFieldName&quot;:&quot;__RequestVerificationToken&quot;,&quot;headerName&quot;:&quot;RequestVerificationToken&quot;,&quot;cookieName&quot;:&quot;HX-XSRF-TOKEN&quot;}}">
-                    <title>Home</title>
-                </head>
-                <body>
-                    <h1>Hello, world!</h1>
-                </body>
-                </html>
-                """);
+            s.ContentShouldBeHtml(FullPageContent("<h1>Hello, world!</h1>", "Home"));
         });
     }
 
