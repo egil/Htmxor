5404cb30973e53007f72370e352f939a0df1a243
Michael Tanczos <mike@logic-gate.com>
2024-04-20 07:04:43 -0400

    feat: Adds response header handling and unit tests (#12)
    Co-authored-by: Egil Hansen <egil@assimilated.dk>

---
+++ src/Htmxor/Http/Models/AjaxContext.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Htmxor.Http.Models;
+
+/// <summary>
+/// Represents a location event with various properties.
+/// </summary>
+public class AjaxContext
+{
+    /// <summary>
+    /// Gets or sets the source element of the request.
+    /// </summary>
+    [JsonPropertyName("source")]
+	public string? Source { get; set; }
+
+	/// <summary>
+	/// Gets or sets an event that "triggered" the request.
+	/// </summary>
+	[JsonPropertyName("event")]
+	public string? Event { get; set; }
+
+	/// <summary>
+	/// Gets or sets a callback that will handle the response HTML.
+	/// </summary>
+	[JsonPropertyName("handler")]
+	public string? Handler { get; set; }
+
+	/// <summary>
+	/// Gets or sets the target to swap the response into.
+	/// </summary>
+	[JsonPropertyName("target")]
+	public string? Target { get; set; }
+
+	/// <summary>
+	/// Gets or sets how the response will be swapped in relative to the target.
+	/// </summary>
+	[JsonPropertyName("swap")]
+	public string? Swap { get; set; }
+
+	/// <summary>
+	/// Gets or sets values to submit with the request.
+	/// </summary>
+	[JsonPropertyName("values")]
+	public string? Values { get; set; }
+
+	/// <summary>
+	/// Gets or sets headers to submit with the request.
+	/// </summary>
+	[JsonPropertyName("headers")]
+	public string? Headers { get; set; }
+
+	/// <summary>
+	/// Gets or sets allows you to select the content you want swapped from a response.
+	/// </summary>
+	[JsonPropertyName("select")]
+	public string? Select { get; set; }
+}
+
---
+++ test/Htmxor.Tests/Http/Mock/MockHttpRequest.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Htmxor.Http.Mock;
+
+/// <summary>
+///     Mock implementation of the <see cref="HttpRequest" /> class for testing purposes.
+/// </summary>
+public class MockHttpRequest : HttpRequest
+{
+    /// <inheritdoc />
+    public override IHeaderDictionary Headers { get; } = new MockHeaderDictionary();
+
+    /// <inheritdoc />
+    public override HttpContext HttpContext { get; } = default!;
+
+    /// <inheritdoc />
+    public override string Method { get; set; } = default!;
+
+    /// <inheritdoc />
+    public override string Scheme { get; set; } = default!;
+
+    /// <inheritdoc />
+    public override bool IsHttps { get; set; } = default!;
+
+    /// <inheritdoc />
+    public override HostString Host { get; set; } = default!;
+
+    /// <inheritdoc />
+    public override PathString PathBase { get; set; }
+
+    /// <inheritdoc />
+    public override PathString Path { get; set; }
+
+    /// <inheritdoc />
+    public override QueryString QueryString { get; set; }
+
+    /// <inheritdoc />
+    public override IQueryCollection Query { get; set; } = default!;
+
+    /// <inheritdoc />
+    public override string Protocol { get; set; } = default!;
+
+    /// <inheritdoc />
+    public override IRequestCookieCollection Cookies { get; set; } = default!;
+
+    /// <inheritdoc />
+    public override long? ContentLength { get; set; }
+
+    /// <inheritdoc />
+    public override string? ContentType { get; set; }
+
+    /// <inheritdoc />
+    public override Stream Body { get; set; } = default!;
+
+    /// <inheritdoc />
+    public override bool HasFormContentType { get; } = default!;
+
+    /// <inheritdoc />
+    public override IFormCollection Form { get; set; } = default!;
+
+    /// <inheritdoc />
+    public override Task<IFormCollection> ReadFormAsync(CancellationToken cancellationToken = default)
+    {
+        // This method is not implemented in the mock. You may need to implement it based on your testing requirements.
+        throw new NotImplementedException();
+    }
+}
