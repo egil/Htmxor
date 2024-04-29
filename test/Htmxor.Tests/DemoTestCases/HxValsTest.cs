fae6904046e4927b5fb21cd0271455873e340c15
Egil Hansen <egil@assimilated.dk>
2024-04-29 13:38:15 +0000

    tests: validate hx-vals, hx-headers, hx-on works

---
+++ test/Htmxor.Tests/DemoTestCases/HxValsTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Htmxor.DemoTestCases;
+
+public class HxValsTest : TestAppTestBase
+{
+    public HxValsTest(TestAppFixture fixture) : base(fixture)
+    {
+    }
+
+    [Fact]
+    public async Task HxVals_correctly_escaped_by_renderer()
+    {
+        await Host.Scenario(s =>
+        {
+            s.Get.Url("/hx-vals-escaped");
+
+            s.StatusCodeShouldBe(HttpStatusCode.OK);
+            s.ContentShouldBeHtml(FullPageContent($$"""
+                <button hx-post="/hx-vals-escaped"
+                        hx-vals='{"myVal": "My Value"}'
+                        type="button">
+                    VALS
+                </button>
+                """));
+        });
+    }
+}
