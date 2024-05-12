803a848f8450f9165b373af1e7415f20815bdb04
Michael Tanczos <mike@logic-gate.com>
2024-05-11 21:06:54 -0400

    style: reformat test files for readability
    Reformatted the `TriggerBuilderTests.cs` and `TriggerSpecificationCacheTests.cs` test files to improve code readability. No changes were made to the logic or functionality of the tests.

--- test/Htmxor.Tests/Http/TriggerBuilderTests.cs
+++ test/Htmxor.Tests/Http/TriggerBuilderTests.cs
@@ -8,254 +8,254 @@ namespace Htmxor.Http;
 
 public class TriggerBuilderTests
 {
-	[Fact]
-	public void TriggerBuilder_OnEvent_CreatesCorrectTrigger()
-	{
-		// Arrange
-		var eventName = "click";
+    [Fact]
+    public void TriggerBuilder_OnEvent_CreatesCorrectTrigger()
+    {
+        // Arrange
+        var eventName = "click";
 
-		// Act
-		var builder = new TriggerBuilder().OnEvent(eventName);
-		var (triggerString, _) = builder.Build();
+        // Act
+        var builder = new TriggerBuilder().OnEvent(eventName);
+        var (triggerString, _) = builder.Build();
 
-		// Assert
-		triggerString.Should().Be("click");
-	}
+        // Assert
+        triggerString.Should().Be("click");
+    }
 
-	[Fact]
-	public void TriggerBuilder_Sse_CreatesCorrectTrigger()
-	{
-		// Arrange
-		var sseEvent = "message";
+    [Fact]
+    public void TriggerBuilder_Sse_CreatesCorrectTrigger()
+    {
+        // Arrange
+        var sseEvent = "message";
 
-		// Act
-		var builder = new TriggerBuilder().Sse(sseEvent);
-		var (triggerString, _) = builder.Build();
+        // Act
+        var builder = new TriggerBuilder().Sse(sseEvent);
+        var (triggerString, _) = builder.Build();
 
-		// Assert
-		triggerString.Should().Be("sse sseEvent:message");
-	}
+        // Assert
+        triggerString.Should().Be("sse sseEvent:message");
+    }
 
-	[Fact]
-	public void TriggerBuilder_Load_CreatesCorrectTrigger()
-	{
-		// Arrange
-		// Act
-		var builder = new TriggerBuilder().Load();
-		var (triggerString, _) = builder.Build();
+    [Fact]
+    public void TriggerBuilder_Load_CreatesCorrectTrigger()
+    {
+        // Arrange
+        // Act
+        var builder = new TriggerBuilder().Load();
+        var (triggerString, _) = builder.Build();
 
-		// Assert
-		triggerString.Should().Be("load");
-	}
+        // Assert
+        triggerString.Should().Be("load");
+    }
 
-	[Fact]
-	public void TriggerBuilder_Revealed_CreatesCorrectTrigger()
-	{
-		// Arrange
-		// Act
-		var builder = new TriggerBuilder().Revealed();
-		var (triggerString, _) = builder.Build();
+    [Fact]
+    public void TriggerBuilder_Revealed_CreatesCorrectTrigger()
+    {
+        // Arrange
+        // Act
+        var builder = new TriggerBuilder().Revealed();
+        var (triggerString, _) = builder.Build();
 
-		// Assert
-		triggerString.Should().Be("revealed");
-	}
+        // Assert
+        triggerString.Should().Be("revealed");
+    }
 
-	[Fact]
-	public void TriggerBuilder_Intersect_CreatesCorrectTrigger()
-	{
-		// Arrange
-		var root = ".container";
-		var threshold = 0.5f;
+    [Fact]
+    public void TriggerBuilder_Intersect_CreatesCorrectTrigger()
+    {
+        // Arrange
+        var root = ".container";
+        var threshold = 0.5f;
 
-		// Act
-		var builder = new TriggerBuilder().Intersect(root, threshold);
-		var (triggerString, _) = builder.Build();
+        // Act
+        var builder = new TriggerBuilder().Intersect(root, threshold);
+        var (triggerString, _) = builder.Build();
 
-		// Assert
-		triggerString.Should().Be("intersect root:.container threshold:0.5");
-	}
+        // Assert
+        triggerString.Should().Be("intersect root:.container threshold:0.5");
+    }
 
-	[Fact]
-	public void TriggerBuilder_Every_CreatesCorrectTrigger()
-	{
-		// Arrange
-		var interval = TimeSpan.FromSeconds(5);
+    [Fact]
+    public void TriggerBuilder_Every_CreatesCorrectTrigger()
+    {
+        // Arrange
+        var interval = TimeSpan.FromSeconds(5);
 
-		// Act
-		var builder = new TriggerBuilder().Every(interval);
-		var (triggerString, _) = builder.Build();
+        // Act
+        var builder = new TriggerBuilder().Every(interval);
+        var (triggerString, _) = builder.Build();
 
-		// Assert
-		triggerString.Should().Be("every 5s");
-	}
+        // Assert
+        triggerString.Should().Be("every 5s");
+    }
 
-	[Fact]
-	public void TriggerBuilder_Custom_CreatesCorrectTrigger()
-	{
-		// Arrange
-		var customTrigger = "custom-event delay:2s";
+    [Fact]
+    public void TriggerBuilder_Custom_CreatesCorrectTrigger()
+    {
+        // Arrange
+        var customTrigger = "custom-event delay:2s";
 
-		// Act
-		var builder = new TriggerBuilder().Custom(customTrigger);
-		var (triggerString, _) = builder.Build();
+        // Act
+        var builder = new TriggerBuilder().Custom(customTrigger);
+        var (triggerString, _) = builder.Build();
 
-		// Assert
-		triggerString.Should().Be("custom-event delay:2s");
-	}
+        // Assert
+        triggerString.Should().Be("custom-event delay:2s");
+    }
 
-	[Fact]
-	public void TriggerModifierBuilder_WithCondition_AddsCorrectCondition()
-	{
-		// Arrange
-		var builder = new TriggerBuilder().OnEvent("click");
+    [Fact]
+    public void TriggerModifierBuilder_WithCondition_AddsCorrectCondition()
+    {
+        // Arrange
+        var builder = new TriggerBuilder().OnEvent("click");
 
-		// Act
-		var (triggerString, modifiers) = builder.WithCondition("ctrlKey").Build();
+        // Act
+        var (triggerString, modifiers) = builder.WithCondition("ctrlKey").Build();
 
-		// Assert
-		triggerString.Should().Be("click[ctrlKey]");
-	}
+        // Assert
+        triggerString.Should().Be("click[ctrlKey]");
+    }
 
-	[Fact]
-	public void TriggerModifierBuilder_Once_AddsOnceModifier()
-	{
-		// Arrange
-		var builder = new TriggerBuilder().OnEvent("click");
+    [Fact]
+    public void TriggerModifierBuilder_Once_AddsOnceModifier()
+    {
+        // Arrange
+        var builder = new TriggerBuilder().OnEvent("click");
 
-		// Act
-		var (triggerString, modifiers) = builder.Once().Build();
+        // Act
+        var (triggerString, modifiers) = builder.Once().Build();
 
-		// Assert
-		triggerString.Should().Be("click once");
-	}
+        // Assert
+        triggerString.Should().Be("click once");
+    }
 
-	[Fact]
-	public void TriggerModifierBuilder_Changed_AddsChangedModifier()
-	{
-		// Arrange
-		var builder = new TriggerBuilder().OnEvent("keyup");
+    [Fact]
+    public void TriggerModifierBuilder_Changed_AddsChangedModifier()
+    {
+        // Arrange
+        var builder = new TriggerBuilder().OnEvent("keyup");
 
-		// Act
-		var (triggerString, modifiers) = builder.Changed().Build();
+        // Act
+        var (triggerString, modifiers) = builder.Changed().Build();
 
-		// Assert
-		triggerString.Should().Be("keyup changed");
-	}
+        // Assert
+        triggerString.Should().Be("keyup changed");
+    }
 
-	[Fact]
-	public void TriggerModifierBuilder_Delay_AddsDelayModifier()
-	{
-		// Arrange
-		var builder = new TriggerBuilder().OnEvent("click");
+    [Fact]
+    public void TriggerModifierBuilder_Delay_AddsDelayModifier()
+    {
+        // Arrange
+        var builder = new TriggerBuilder().OnEvent("click");
 
-		// Act
-		var (triggerString, modifiers) = builder.Delay(TimeSpan.FromSeconds(1)).Build();
+        // Act
+        var (triggerString, modifiers) = builder.Delay(TimeSpan.FromSeconds(1)).Build();
 
-		// Assert
-		triggerString.Should().Be("click delay:1s");
-	}
+        // Assert
+        triggerString.Should().Be("click delay:1s");
+    }
 
-	[Fact]
-	public void TriggerModifierBuilder_Throttle_AddsThrottleModifier()
-	{
-		// Arrange
-		var builder = new TriggerBuilder().OnEvent("click");
+    [Fact]
+    public void TriggerModifierBuilder_Throttle_AddsThrottleModifier()
+    {
+        // Arrange
+        var builder = new TriggerBuilder().OnEvent("click");
 
-		// Act
-		var (triggerString, modifiers) = builder.Throttle(TimeSpan.FromSeconds(2)).Build();
+        // Act
+        var (triggerString, modifiers) = builder.Throttle(TimeSpan.FromSeconds(2)).Build();
 
-		// Assert
-		triggerString.Should().Be("click throttle:2s");
-	}
+        // Assert
+        triggerString.Should().Be("click throttle:2s");
+    }
 
-	[Fact]
-	public void TriggerModifierBuilder_From_AddsFromModifier()
-	{
-		// Arrange
-		var builder = new TriggerBuilder().OnEvent("keydown");
+    [Fact]
+    public void TriggerModifierBuilder_From_AddsFromModifier()
+    {
+        // Arrange
+        var builder = new TriggerBuilder().OnEvent("keydown");
 
-		// Act
-		var (triggerString, modifiers) = builder.From("document").Build();
+        // Act
+        var (triggerString, modifiers) = builder.From("document").Build();
 
-		// Assert
-		triggerString.Should().Be("keydown from:document");
-	}
+        // Assert
+        triggerString.Should().Be("keydown from:document");
+    }
 
-	[Fact]
-	public void TriggerModifierBuilder_Target_AddsTargetModifier()
-	{
-		// Arrange
-		var builder = new TriggerBuilder().OnEvent("click");
+    [Fact]
+    public void TriggerModifierBuilder_Target_AddsTargetModifier()
+    {
+        // Arrange
+        var builder = new TriggerBuilder().OnEvent("click");
 
-		// Act
-		var (triggerString, _) = builder.Target(".child").Build();
+        // Act
+        var (triggerString, _) = builder.Target(".child").Build();
 
-		// Assert
-		triggerString.Should().Be("click target:.child");
-	}
+        // Assert
+        triggerString.Should().Be("click target:.child");
+    }
 
-	[Fact]
-	public void TriggerModifierBuilder_Consume_AddsConsumeModifier()
-	{
-		// Arrange
-		var builder = new TriggerBuilder().OnEvent("click");
+    [Fact]
+    public void TriggerModifierBuilder_Consume_AddsConsumeModifier()
+    {
+        // Arrange
+        var builder = new TriggerBuilder().OnEvent("click");
 
-		// Act
-		var (triggerString, _) = builder.Consume().Build();
+        // Act
+        var (triggerString, _) = builder.Consume().Build();
 
-		// Assert
-		triggerString.Should().Be("click consume");
-	}
+        // Assert
+        triggerString.Should().Be("click consume");
+    }
 
-	[Fact]
-	public void TriggerModifierBuilder_Queue_AddsQueueModifier()
-	{
-		// Arrange
-		var builder = new TriggerBuilder().OnEvent("click");
+    [Fact]
+    public void TriggerModifierBuilder_Queue_AddsQueueModifier()
+    {
+        // Arrange
+        var builder = new TriggerBuilder().OnEvent("click");
 
-		// Act
-		var (triggerString, _) = builder.Queue(TriggerQueueOption.All).Build();
+        // Act
+        var (triggerString, _) = builder.Queue(TriggerQueueOption.All).Build();
 
-		// Assert
-		triggerString.Should().Be("click queue:all");
-	}
+        // Assert
+        triggerString.Should().Be("click queue:all");
+    }
 
-	[Fact]
-	public void TriggerBuilder_ChainedOperations_AddsCorrectModifiers()
-	{
-		// Arrange
-		var builder = new TriggerBuilder();
+    [Fact]
+    public void TriggerBuilder_ChainedOperations_AddsCorrectModifiers()
+    {
+        // Arrange
+        var builder = new TriggerBuilder();
 
-		// Act
-		var (triggerString, _) = builder
-			.OnEvent("click")
-			.WithCondition("shiftKey")
-			.Delay(TimeSpan.FromMilliseconds(500))
-			.Throttle(TimeSpan.FromMilliseconds(200))
-			.From("window")
-			.Target("#specificElement")
-			.Consume()
-			.Queue(TriggerQueueOption.First)
-			.Build();
+        // Act
+        var (triggerString, _) = builder
+            .OnEvent("click")
+            .WithCondition("shiftKey")
+            .Delay(TimeSpan.FromMilliseconds(500))
+            .Throttle(TimeSpan.FromMilliseconds(200))
+            .From("window")
+            .Target("#specificElement")
+            .Consume()
+            .Queue(TriggerQueueOption.First)
+            .Build();
 
-		// Assert
-		triggerString.Should().Be("click[shiftKey] delay:500ms throttle:200ms from:window target:#specificElement consume queue:first");
-	}
+        // Assert
+        triggerString.Should().Be("click[shiftKey] delay:500ms throttle:200ms from:window target:#specificElement consume queue:first");
+    }
 
-	[Fact]
-	public void TriggerBuilder_MultipleTriggers_AddsCorrectModifiers()
-	{
-		// Arrange
-		var builder = new TriggerBuilder();
+    [Fact]
+    public void TriggerBuilder_MultipleTriggers_AddsCorrectModifiers()
+    {
+        // Arrange
+        var builder = new TriggerBuilder();
 
-		// Act
-		var (triggerString, _) = builder
-			.OnEvent("click")
-			.Or()
-			.Load()
-			.Build();
+        // Act
+        var (triggerString, _) = builder
+            .OnEvent("click")
+            .Or()
+            .Load()
+            .Build();
 
-		// Assert
-		triggerString.Should().Be("click, load");
-	}
+        // Assert
+        triggerString.Should().Be("click, load");
+    }
 }
--- test/Htmxor.Tests/Http/TriggerSpecificationCacheTests.cs
+++ test/Htmxor.Tests/Http/TriggerSpecificationCacheTests.cs
@@ -1,112 +1,108 @@
-using System;
-using System.Collections.Generic;
 using System.Text.Json;
-using Xunit;
-using FluentAssertions;
-using FluentAssertions.Primitives;
+using Htmxor.TestAssets.FluentAssertions;
 
 namespace Htmxor.Http;
 
 public class TriggerSpecificationCacheTests
 {
-	[Fact]
-	public void TriggerSpecificationCache_Revealed_ReturnsCorrectJson()
-	{
-		// Arrange
-		var cache = new TriggerSpecificationCache(
-			Trigger.Revealed()
-		);
+    [Fact]
+    public void TriggerSpecificationCache_Revealed_ReturnsCorrectJson()
+    {
+        // Arrange
+        var cache = new TriggerSpecificationCache(
+            Trigger.Revealed()
+        );
 
-		// Act
-		var json = JsonSerializer.Serialize(new { triggerSpecsCache = cache },
-			new JsonSerializerOptions { DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull });
+        // Act
+        var json = JsonSerializer.Serialize(new { triggerSpecsCache = cache },
+            new JsonSerializerOptions { DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull });
 
-		// Assert
-		json.Should().BeJsonSemanticallyEqualTo(
-			"{\"triggerSpecsCache\":{\"revealed\":[{\"trigger\":\"revealed\"}]}}"
-		);
-	}
+        // Assert
+        json.Should().BeJsonSemanticallyEqualTo(
+            "{\"triggerSpecsCache\":{\"revealed\":[{\"trigger\":\"revealed\"}]}}"
+        );
+    }
 
-	[Fact]
-	public void TriggerSpecificationCache_OnEventWithFrom_ReturnsCorrectJson()
-	{
-		// Arrange
-		var cache = new TriggerSpecificationCache(
-			Trigger.OnEvent("newContact").From("body")
-		);
+    [Fact]
+    public void TriggerSpecificationCache_OnEventWithFrom_ReturnsCorrectJson()
+    {
+        // Arrange
+        var cache = new TriggerSpecificationCache(
+            Trigger.OnEvent("newContact").From("body")
+        );
 
-		// Act
-		var json = JsonSerializer.Serialize(new { triggerSpecsCache = cache },
-			new JsonSerializerOptions { DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull });
+        // Act
+        var json = JsonSerializer.Serialize(new { triggerSpecsCache = cache },
+            new JsonSerializerOptions { DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull });
 
-		// Assert
-		json.Should().BeJsonSemanticallyEqualTo(
-			"{\"triggerSpecsCache\":{\"newContact from:body\":[{\"trigger\":\"newContact\",\"from\":\"body\"}]}}"
-		);
-	}
+        // Assert
+        json.Should().BeJsonSemanticallyEqualTo(
+            "{\"triggerSpecsCache\":{\"newContact from:body\":[{\"trigger\":\"newContact\",\"from\":\"body\"}]}}"
+        );
+    }
 
-	[Fact]
-	public void TriggerSpecificationCache_OnEventWithMultipleModifiers_ReturnsCorrectJson()
-	{
-		// Arrange
-		var cache = new TriggerSpecificationCache(
-			Trigger.OnEvent("keyup").Changed().Delay(TimeSpan.FromMilliseconds(500))
-				.Or()
-				.OnEvent("mouseenter").Once()
-		);
+    [Fact]
+    public void TriggerSpecificationCache_OnEventWithMultipleModifiers_ReturnsCorrectJson()
+    {
+        // Arrange
+        var cache = new TriggerSpecificationCache(
+            Trigger.OnEvent("keyup").Changed().Delay(TimeSpan.FromMilliseconds(500))
+                .Or()
+                .OnEvent("mouseenter").Once()
+        );
 
-		// Act
-		var json = JsonSerializer.Serialize(new { triggerSpecsCache = cache },
-			new JsonSerializerOptions { DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull });
+        // Act
+        var json = JsonSerializer.Serialize(new { triggerSpecsCache = cache },
+            new JsonSerializerOptions { DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull });
 
-		// Assert
-		json.Should().BeJsonSemanticallyEqualTo(
-			"{\"triggerSpecsCache\":{\"keyup changed delay:500ms, mouseenter once\":[{\"trigger\":\"keyup\",\"changed\":true,\"delay\":500},{\"trigger\":\"mouseenter\",\"once\":true}]}}"
-		);
-	}
+        // Assert
+        json.Should().BeJsonSemanticallyEqualTo(
+            "{\"triggerSpecsCache\":{\"keyup changed delay:500ms, mouseenter once\":[{\"trigger\":\"keyup\",\"changed\":true,\"delay\":500},{\"trigger\":\"mouseenter\",\"once\":true}]}}"
+        );
+    }
 
-	[Fact]
-	public void TriggerSpecificationCache_EveryAndOnEvent_ReturnsCorrectJson()
-	{
-		// Arrange
-		var cache = new TriggerSpecificationCache(
-			Trigger.Every(TimeSpan.FromSeconds(30))
-				.Or()
-				.OnEvent("newContact").From("closest (form input)")
-		);
+    [Fact]
+    public void TriggerSpecificationCache_EveryAndOnEvent_ReturnsCorrectJson()
+    {
+        // Arrange
+        var cache = new TriggerSpecificationCache(
+            Trigger.Every(TimeSpan.FromSeconds(30))
+                .Or()
+                .OnEvent("newContact").From("closest (form input)")
+        );
 
-		// Act
-		var json = JsonSerializer.Serialize(new { triggerSpecsCache = cache },
-			new JsonSerializerOptions { DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull });
+        // Act
+        var json = JsonSerializer.Serialize(new { triggerSpecsCache = cache },
+            new JsonSerializerOptions { DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull });
 
-		// Assert
-		json.Should().BeJsonSemanticallyEqualTo(
-			"{\"triggerSpecsCache\":{\"every 30s, newContact from:closest (form input)\":[{\"trigger\":\"every\",\"pollInterval\":30000},{\"trigger\":\"newContact\",\"from\":\"closest (form input)\"}]}}"
-		);
-	}
+        // Assert
+        json.Should().BeJsonSemanticallyEqualTo(
+            "{\"triggerSpecsCache\":{\"every 30s, newContact from:closest (form input)\":[{\"trigger\":\"every\",\"pollInterval\":30000},{\"trigger\":\"newContact\",\"from\":\"closest (form input)\"}]}}"
+        );
+    }
 
-	[Fact]
-	public void TriggerSpecificationCache_ComplexTriggers_ReturnsCorrectJson()
-	{
-		// Arrange
-		var cache = new TriggerSpecificationCache(
-			Trigger.Revealed(),
-			Trigger.OnEvent("newContact").From("body"),
-			Trigger.OnEvent("keyup").Changed().Delay(TimeSpan.FromMilliseconds(500))
-				.Or()
-				.OnEvent("mouseenter").Once(),
-			Trigger.Every(TimeSpan.FromSeconds(30))
-				.Or()
-				.OnEvent("newContact").From("closest (form input)")
-		);
+    [Fact]
+    public void TriggerSpecificationCache_ComplexTriggers_ReturnsCorrectJson()
+    {
+        // Arrange
+        var cache = new TriggerSpecificationCache(
+            Trigger.Revealed(),
+            Trigger.OnEvent("newContact").From("body"),
+            Trigger.OnEvent("keyup").Changed().Delay(TimeSpan.FromMilliseconds(500))
+                .Or()
+                .OnEvent("mouseenter").Once(),
+            Trigger.Every(TimeSpan.FromSeconds(30))
+                .Or()
+                .OnEvent("newContact").From("closest (form input)")
+        );
 
-		// Act
-		var json = JsonSerializer.Serialize(new { triggerSpecsCache = cache },
-			new JsonSerializerOptions { DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull });
+        // Act
+        var json = JsonSerializer.Serialize(new { triggerSpecsCache = cache },
+            new JsonSerializerOptions { DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull });
 
-		// Assert
-		json.Should().BeJsonSemanticallyEqualTo(
-			"{\"triggerSpecsCache\":{\"revealed\":[{\"trigger\":\"revealed\"}],\"newContact from:body\":[{\"trigger\":\"newContact\",\"from\":\"body\"}],\"keyup changed delay:500ms, mouseenter once\":[{\"trigger\":\"keyup\",\"changed\":true,\"delay\":500},{\"trigger\":\"mouseenter\",\"once\":true}],\"every 30s, newContact from:closest (form input)\":[{\"trigger\":\"every\",\"pollInterval\":30000},{\"trigger\":\"newContact\",\"from\":\"closest (form input)\"}]}}"
-		);
-	}
+        // Assert
+        json.Should().BeJsonSemanticallyEqualTo(
+            "{\"triggerSpecsCache\":{\"revealed\":[{\"trigger\":\"revealed\"}],\"newContact from:body\":[{\"trigger\":\"newContact\",\"from\":\"body\"}],\"keyup changed delay:500ms, mouseenter once\":[{\"trigger\":\"keyup\",\"changed\":true,\"delay\":500},{\"trigger\":\"mouseenter\",\"once\":true}],\"every 30s, newContact from:closest (form input)\":[{\"trigger\":\"every\",\"pollInterval\":30000},{\"trigger\":\"newContact\",\"from\":\"closest (form input)\"}]}}"
+        );
+    }
 }
