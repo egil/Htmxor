c82b14147e42c5c055a4ce70391209120e131a69
Michael Tanczos <mike@logic-gate.com>
2024-05-14 18:31:09 -0400

    fix(serialization): replace JsonCamelCaseStringEnumConverter with custom SwapStyleEnumConverter
    Fixes #51 - This commit introduces a new `SwapStyleEnumConverter` to handle the serialization of the `SwapStyle` enum. This change also updates the test case in `HtmxHeadOutletTest.cs` to use correct serialized value.

---
+++ src/Htmxor/Serialization/SwapStyleEnumConverter.cs
@@ -0,0 +1,35 @@
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Htmxor.Serialization;
+
+public sealed class SwapStyleEnumConverter : JsonConverter<SwapStyle>
+{
+	public override SwapStyle Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+	{
+		var value = reader.GetString();
+
+		var style = value switch
+		{
+			null => SwapStyle.Default,
+			Constants.SwapStyles.Default => SwapStyle.Default,
+			Constants.SwapStyles.InnerHTML => SwapStyle.InnerHTML,
+			Constants.SwapStyles.OuterHTML => SwapStyle.OuterHTML,
+			Constants.SwapStyles.BeforeBegin => SwapStyle.BeforeBegin,
+			Constants.SwapStyles.AfterBegin => SwapStyle.AfterBegin,
+			Constants.SwapStyles.BeforeEnd => SwapStyle.BeforeEnd,
+			Constants.SwapStyles.AfterEnd => SwapStyle.AfterEnd,
+			Constants.SwapStyles.Delete => SwapStyle.Delete,
+			Constants.SwapStyles.None => SwapStyle.None,
+			_ => throw new SwitchExpressionException(value)
+		};
+
+		return style;
+	}
+
+	public override void Write(Utf8JsonWriter writer, SwapStyle value, JsonSerializerOptions options)
+	{
+		writer?.WriteStringValue(value.ToHtmxString());
+	}
+}
